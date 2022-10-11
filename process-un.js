4957af4fbae722d29fdc701291f2fa182d0b23b9
Brian Grinstead <briangrinstead@gmail.com>
2022-10-11 10:58:34 -0700

    rename

--- process-un.js
+++ process-un.js
@@ -1,22 +1,24 @@
 import papaparse from "https://esm.sh/papaparse/";
 
+// SortOrder,LocID,Notes,ISO3_code,ISO2_code,SDMX_code,LocTypeID,LocTypeName,ParentID,Location,VarID,Variant,Time,TPopulation1Jan,TPopulation1July
 let { data } = papaparse.parse(Deno.readTextFileSync("un-indicators.csv"), {
   dynamicTyping: true,
   header: true,
 });
 
 data = data
-  .filter((row) => row.ISO3_code && row.Time == 2021)
+  .filter((row) => row.ISO3_code && row.Time == 2021);
+
+data = data
   .map((row) => ({
-    Name: row.Location,
+    name: row.Location,
     ISO3: row.ISO3_code,
     ISO2: row.ISO2_code,
-    Population: Math.round(row.TPopulation1July),
+    population: Math.round(row.TPopulation1July),
   }))
   .sort((a, b) => b.Population - a.Population);
 
 console.log(data.slice(0, 10), data.length);
-// SortOrder,LocID,Notes,ISO3_code,ISO2_code,SDMX_code,LocTypeID,LocTypeName,ParentID,Location,VarID,Variant,Time,TPopulation1Jan,TPopulation1July
 
 
 Deno.writeTextFileSync("country_population.csv", papaparse.unparse(data));
